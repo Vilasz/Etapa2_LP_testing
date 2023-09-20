932ff68003e784c013a003a9ccd7e5fddf07ec3e
Joao Felipe Vilas Boas <127103088+Vilasz@users.noreply.github.com>
2023-09-20 09:09:50 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,32 @@
-from main import main
+import unittest
+import doctest
+import locale
+import os
 
-def testing():
-    assert main("25 de agosto de 2023", "31 de dezembro de 2025") == 856
-    assert main("10 de janeiro de 2024", "15 de fevereiro de 2024") == 36
+from funcoes import calculo_digita, calculo_arquivo
+
+locale.setlocale(locale.LC_ALL, "pt_BR.utf8")
+
+class TestCalculoFuncoes(unittest.TestCase):
+    # Não funciona sem return*
+    def test_calculo_digita(self):
+        resultado = calculo_digita("23 de Agosto de 2023 - 18 de Setembro de 2023")
+        self.assertEqual(resultado, "O número de dias entre essas datas é:  25")
+
+        resultado = calculo_digita("18 de Setembro de 2023 - 23 de Agosto de 2023")
+        self.assertEqual(resultado, "O número de dias entre essas datas é:  25")
+
+
+    # Funciona corretamente
+    def test_calculo_arquivo(self):
+        with open("input.txt", "w") as f:
+            f.write("23 de Agosto de 2023 - 18 de Setembro de 2023\n")
+
+
+        resultado = calculo_arquivo("input.txt")
+        self.assertEqual(resultado, "Diferença de dias: 25")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
